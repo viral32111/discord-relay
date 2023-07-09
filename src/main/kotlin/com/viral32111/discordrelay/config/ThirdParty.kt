e093e7dd3ed21c7da6a6403ee12ef308c5f518f2
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-07-09 14:23:15 +0100

    Craftatar is down right now

--- src/main/kotlin/com/viral32111/discordrelay/config/ThirdParty.kt
+++ src/main/kotlin/com/viral32111/discordrelay/config/ThirdParty.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ThirdParty(
 	@Required @SerialName( "profile-url" ) val profileUrl: String = "https://namemc.com/profile/%s",
-	@Required @SerialName( "avatar-url" ) val avatarUrl: String = "https://crafatar.com/avatars/%s.png?size=128&overlay",
+	@Required @SerialName( "avatar-url" ) val avatarUrl: String = "https://mc-heads.net/avatar/%s/128", // https://crafatar.com/avatars/%s.png?size=128&overlay
 	@Required @SerialName( "server-url" ) val serverUrl: String = "https://mcsrvstat.us/server/%s",
 	@Required @SerialName( "ip-address-url" ) val ipAddressUrl: String = "https://ipinfo.io/%s",
 	@Required @SerialName( "proxycheck" ) val proxyCheck: ProxyCheck = ProxyCheck(),
